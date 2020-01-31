e8b3f8eacdeb39bd07caa0d413e6b70a7bc7660a
Allison <vladimiramnell@gmail.com>
2020-01-31 15:25:42 +1000

    third commit

---
+++ client/build/precache-manifest.128e62e8c54dcd34a8d2f0d8d9fd6c22.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002b8de5469960b32981ea87859a031a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "46a6dbcfd8f00f1fa488",
+    "url": "/static/css/2.d88a60dc.chunk.css"
+  },
+  {
+    "revision": "e2ef8d94a49a3561b4df",
+    "url": "/static/css/main.577754b7.chunk.css"
+  },
+  {
+    "revision": "46a6dbcfd8f00f1fa488",
+    "url": "/static/js/2.b55ab51c.chunk.js"
+  },
+  {
+    "revision": "14c9108fd5a38194785d545908fb6cbd",
+    "url": "/static/js/2.b55ab51c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e2ef8d94a49a3561b4df",
+    "url": "/static/js/main.9d8d3800.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
